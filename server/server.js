2985ce9a14bdbcf55db69c3068a247e76f02da41
vaibhavvaid14 <vaibhavvaid2004@gmail.com>
2026-05-15 14:37:36 +0530

    Use Regex for catch-all route to avoid PathError

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   const publicPath = path.join(__dirname, '..', 'client', 'dist');
   app.use(express.static(publicPath));
 
-  app.get('/*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
   });
 } else {
